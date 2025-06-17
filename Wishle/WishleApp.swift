9476e1a48e223430ffb0b945dd5531ae0cff3d28
muhiro_ <muhiro.12@gmail.com>
2025-06-17 23:42:36 +0900

    Initial Commit

---
+++ Wishle/WishleApp.swift
@@ -0,0 +1,32 @@
+//
+//  WishleApp.swift
+//  Wishle
+//
+//  Created by Hiromu Nakano on 2025/06/17.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WishleApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ WishleTests/WishleTests.swift
@@ -0,0 +1,16 @@
+//
+//  WishleTests.swift
+//  WishleTests
+//
+//  Created by Hiromu Nakano on 2025/06/17.
+//
+
+import Testing
+
+struct WishleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
