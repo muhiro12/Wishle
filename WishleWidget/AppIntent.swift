054b7e2fb6f1f01ebb621834b43ecb225f51633a
muhiro_ <muhiro.12@gmail.com>
2025-06-18 00:00:20 +0900

    Add WishleWidget

---
+++ WishleWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  WishleWidget
+//
+//  Created by Hiromu Nakano on 2025/06/17.
+//
+
+import AppIntents
+import WidgetKit
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ WishleWidget/WishleWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  WishleWidgetBundle.swift
+//  WishleWidget
+//
+//  Created by Hiromu Nakano on 2025/06/17.
+//
+
+import SwiftUI
+import WidgetKit
+
+@main
+struct WishleWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WishleWidget()
+        WishleWidgetControl()
+        WishleWidgetLiveActivity()
+    }
+}
